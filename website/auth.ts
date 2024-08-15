883540a196cb71a24ab44d24e079e6c930049d0b
tiagosan040 <tiagosan040@gmail.com>
2024-08-15 15:54:22 -0300

    feat(website): configure prisma

--- website/auth.ts
+++ website/auth.ts
@@ -1,6 +1,16 @@
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from '@/infra/prisma/client';
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Google],
+  callbacks: {
+    async signIn(params) {
+      console.log({ params });
+
+      return true;
+    },
+  },
 });
