290e08904dca85d17aeac07e1d0254fde32b7380
tiagosan040 <tiagosan040@gmail.com>
2024-08-16 14:41:10 -0300

    feat(website): configure custom fonts from local

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -1,19 +1,72 @@
-import { DM_Sans } from 'next/font/google';
-import { Space_Mono } from 'next/font/google';
+import localFont from 'next/font/local';
 import { Metadata } from 'next';
 import './globals.css';
 
-const fontHeading = DM_Sans({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-heading',
+const dmSans = localFont({
+  src: [
+    {
+      path: '../../public/fonts/dm-sans/DMSans-Thin.ttf',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/dm-sans/DMSans-ExtraLight.ttf',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/dm-sans/DMSans-Light.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/dm-sans/DMSans-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/dm-sans/DMSans-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/dm-sans/DMSans-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/dm-sans/DMSans-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/dm-sans/DMSans-ExtraBold.ttf',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/dm-sans/DMSans-Black.ttf',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-heading'
 });
 
-const fontBody = Space_Mono({
-  subsets: ['latin'],
-  display: 'swap',
+const spaceMono = localFont({
+  src: [
+    {
+      path: '../../public/fonts/space-mono/SpaceMono-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/space-mono/SpaceMono-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
   variable: '--font-body',
-  weight: ['400', '700'],
 });
 
 export const metadata: Metadata = {
@@ -32,7 +85,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br" className={`${fontHeading.variable} ${fontBody.variable} antialiased`}>
+    <html lang="pt-br" className={`${dmSans.variable} ${spaceMono.variable} antialiased`}>
       <body>{children}</body>
     </html>
   );
