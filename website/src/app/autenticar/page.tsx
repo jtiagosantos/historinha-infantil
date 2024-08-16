3772afdc827a115f5fab53fca8ecbab9076a43f4
tiagosan040 <tiagosan040@gmail.com>
2024-08-15 23:25:45 -0300

    feat(website): add loading on signIn button

--- website/src/app/autenticar/page.tsx
+++ website/src/app/autenticar/page.tsx
@@ -1,11 +1,21 @@
 'use client';
 
+import { useState } from 'react';
+import { ThreeDots } from 'react-loader-spinner';
 import { Card, CardContent } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { GoogleIcon } from '@/components/icons/google';
 import { signIn } from 'next-auth/react';
 
 export default function Page() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSign = async () => {
+    setIsLoading(true);
+    await signIn('google', { redirectTo: '/' });
+    setIsLoading(false);
+  }
+
   return (
     <div className="flex items-center justify-center h-screen">
       <Card className="w-full max-w-lg py-6 pb-3 px-2 shadow-lg rounded-lg bg-card border border-red-400 border-border max-sm:h-screen max-sm:flex max-sm:items-center max-sm:pb-[80px]">
@@ -20,11 +30,25 @@ export default function Page() {
           <div className="text-center">
             <p className="text-muted-foreground w-full text-left">* Faça login para criar uma história.</p>
             <Button 
-              onClick={() => signIn('google', { redirectTo: '/' })} 
+              disabled={isLoading}
+              onClick={handleSign} 
               className="mt-2 flex items-center justify-center w-full bg-background border border-input text-muted-foreground py-3 rounded-lg hover:bg-accent hover:text-accent-foreground outline-accent"
             >
-              <GoogleIcon className="mr-2 h-5 w-5" />
-              Entrar com Google
+              {!isLoading ? (
+                <>
+                  <GoogleIcon className="mr-2 h-5 w-5" />
+                  Entrar com Google
+                </>
+              ) : (
+                <ThreeDots
+                  height="20"
+                  width="40"
+                  radius="9"
+                  color="#5c4523"
+                  ariaLabel="three-dots-loading"
+                  visible={true}
+                />
+              )}
             </Button>
           </div>
         </CardContent>
