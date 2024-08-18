3fa1c1d7af373369a7878eacce98f7c03b69bcc1
tiagosan040 <tiagosan040@gmail.com>
2024-08-18 20:11:59 -0300

    feat(website): implement util function to get a slice of the story text

--- website/src/app/(home)/story-card.tsx
+++ website/src/app/(home)/story-card.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { formatStoryTextPreview } from '@/utils/format-story-text-preview';
 
 export const StoryCard = () => {
   return (
@@ -10,9 +11,9 @@ export const StoryCard = () => {
       </CardHeader>
       <CardContent className="p-0 mt-5">
         <p className="font-body text-muted-foreground leading-[20px] text-sm min-h-[100px] line-clamp-5">
-          Luca, um menino curioso de 8 anos, adora visitar o parque perto de sua casa. 
+          {formatStoryTextPreview(`Luca, um menino curioso de 8 anos, adora visitar o parque perto de sua casa. 
           Nesta história, ele embarca em uma jornada emocionante, encontrando seus amigos 
-          animais favoritos e descobrindo um tesouro escondido.
+          animais favoritos e descobrindo um tesouro escondido.`)}
         </p>
         <Link href="/historia/mocked-id" className="w-full text-right block mt-2 text-[15px] hover:underline hover:text-accent underline-offset-4">
           Ler história
